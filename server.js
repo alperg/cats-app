ab8d487e9bf96e62cf4620cb2adbbc23b9c65ff7
Alper Gokcehan <mail@alperg.com>
2021-03-08 21:18:03 -0500

    created a few linting errors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+const app = express()
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static('public'));
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-const exphbs = require('express-handlebars');
+const exphbs_obj = require('express-handlebars');
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs_obj({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
